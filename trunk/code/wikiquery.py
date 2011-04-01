029faa1e832651e921e774e3e72065edb4a6f37d
tathagatadg@gmail.com <tathagatadg@gmail.com@1287879c-c4bf-d38b-8da1-84e1c4f3f8ab>
2011-04-01 02:53:56 +0000

    Adding the code

---
+++ trunk/code/wikiquery.py
@@ -0,0 +1,125 @@
+from __future__ import division
+from nltk.corpus import stopwords
+import urllib
+import pprint
+import simplejson
+import re
+
+url = 'http://en.wikipedia.org/w/api.php?action=list'
+#
+debug = False
+regex_tokenize = False
+def stop_removed():
+	stopset = set(stopwords.words('english'))
+	
+	sentences = {}
+	with open('DU_TU.txt') as f:
+		for line in f:			
+			du = line.split('\t')[0]
+	    		tu = line.split('\t')[1]
+			#print du, tu
+			tu_stop_removed = ''
+	        	for word in tu.split(' '):
+	            		if word not in stopset:
+	                		tu_stop_removed +=word+' '
+			sentences.update({du:tu_stop_removed}) 
+#	print sentences
+	return sentences
+
+if __name__=="__main__":
+	topic_units = stop_removed()
+	for doubt_unit, topic_unit in topic_units.iteritems():
+	 	print doubt_unit,';',topic_unit.rstrip("\n")
+
+
+#http://en.wikipedia.org/w/api.php?
+#action=query
+#&list=search
+#&srprop=size|wordcount|timestamp|score|snippet|titlesnippet|redirecttitle|redirectsnippet|sectiontitle|sectionsnippet|hasrelated
+#&srsearch=SEARCH STRING GOES HERE
+#srwhat=text|title|nearsearch (only one value is allowed)
+#
+#&format=json
+#
+		url = 'http://en.wikipedia.org/w/api.php?action=query&list=search'
+		srch_args = {'format':'json',
+		      #'srprop':'size|wordcount|timestamp|score|snippet|titlesnippet|redirecttitle|redirectsnippet|sectiontitle|sectionsnippet|hasrelated',
+		      'srprop':'titlesnippet|snippet',
+			'srsearch':topic_unit,
+			'srwhat':'text',	
+			}
+		url_srch = url + '&' + urllib.urlencode(srch_args)
+		#print url_srch
+		results = simplejson.load(urllib.urlopen(url_srch))
+		if debug:
+			pprint.PrettyPrinter(indent=4).pprint(results)
+
+		for result in results['query'].get('search',()):
+			#SNIPPETS
+			#TODO include distance between matches in the weight
+			snippet_split_weights = {}
+			snippet_splits = re.split(r'<b>...</b>',result['snippet'] )
+			for snippet_split in snippet_splits:
+				snippet_split_len = len(snippet_split)
+				snippet_split_match_count = len(re.findall(r'<span class=\'searchmatch\'>\w+</span>',snippet_split))
+				snippet_split_weights[snippet_splits.index(snippet_split)] = snippet_split_match_count/snippet_split_len
+			snippet_weight = 0
+			for snippet_split_weight in snippet_split_weights.values():
+				snippet_weight +=snippet_split_weight
+			snippet_weight= 0.33*snippet_weight			
+			
+			#TITLE
+			title_match_count = len(re.findall(r'<span class=\'searchmatch\'>\w+<\/span>',result['titlesnippet']))
+			title_len = len(result['title'])
+			title_weight = title_match_count/title_len
+
+			#TODO need to determine appropriate wieghts
+			overall_result_weight = (title_weight+snippet_weight)/2
+			
+			#print '\t',snippets_splits.encode("utf-8")
+
+			snippet = re.sub(r'<.*?>','',result['snippet'])
+			
+			if regex_tokenize:
+				from nltk.tokenize import RegexpTokenizer
+        			tokenizer = RegexpTokenizer('([A-Z]\.)+|\w+|\$[\d\.]+')
+			
+				snippet_tokens = set(tokenizer.tokenize(snippet))
+				title_tokens = set(tokenizer.tokenize(result['title']))
+				doubt_unit_tokens = set(tokenizer.tokenize(doubt_unit))
+			else:
+				snippet_tokens = set((snippet).lower().split(' '))
+				title_tokens = set((result['title']).lower().split(' '))
+				doubt_unit_tokens = set(doubt_unit.lower().split(' '))
+
+				
+			found = False
+			
+			if snippet_tokens.intersection(doubt_unit_tokens):
+				print "S",';',title_weight,';',snippet_weight,';',overall_result_weight 
+				found = True
+			if title_tokens.intersection(doubt_unit_tokens):
+				print "T",';',title_weight,';',snippet_weight,';',overall_result_weight
+				found = True
+			if found == False:
+				print "N",';',title_weight,';',snippet_weight,';',overall_result_weight
+   		print "="*50	
+
+def rte_features(rtepair):
+    extractor = nltk.RTEFeatureExtractor(rtepair)
+    features = {}
+    features['word_overlap'] = len(extractor.overlap('word'))
+    features['word_hyp_extra'] = len(extractor.hyp_extra('word'))
+    features['ne_overlap'] = len(extractor.overlap('ne'))
+    features['ne_hyp_extra'] = len(extractor.hyp_extra('ne'))
+    return features
+
+
+def POS_tag():
+	for line in open('doubtful_statement.txt'):
+	        a,b,c,d,e,f = map(str, line.split('\t'))
+	        text1 = nltk.word_tokenize(b)
+	        text2 = nltk.word_tokenize(c)
+	        print nltk.pos_tag(text1)
+	        print nltk.pos_tag(text2)
+	
